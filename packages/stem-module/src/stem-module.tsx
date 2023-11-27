8442fbeeec78486e1f48835166bc5a0c04006e97
BartWtje <bartw.tje@hotmail.com>
2023-11-27 11:34:04 +0100

    Meeste onderdelen verwerkt; alleen border rond gekozen afbeelding mist nog

--- packages/stem-module/src/stem-module.tsx
+++ packages/stem-module/src/stem-module.tsx
@@ -35,93 +35,109 @@ export function StemModule(
         />
 
         {currentStep === 0 ? (
-          <ResourceOverview
-            className="stem-module-resource-container"
-            renderHeader={() => (
-              <>
-                <p className="stem-module-resource-container-intro">
-                  Kies uit onderstaand overzicht jouw favoriete ontwerp voor het
-                  kunstwerk voor de Aletta Jacobsbuurt, en vul in de volgende
-                  stap je gegevens in.
-                </p>
-
-                <h6>Kies een ontwerp</h6>
-                <Spacer size={1} />
-              </>
-            )}
-            renderItem={(resource) => (
-              <>
-                <Image
-                  src={resource.images[0]}
-                  imageFooter={
-                    <>
-                      <PlainButton>Lees meer</PlainButton>
-                      <SecondaryButton onClick={() => setResource(resource)}>
-                        Stem
-                      </SecondaryButton>
-                    </>
-                  }
-                />
-              </>
-            )}
-            {...props}
-            allowFiltering={false}
-          />
+          <div>
+            <ResourceOverview
+              className="stem-module-resource-container"
+              renderHeader={() => (
+                <>
+                  <p className="stem-module-resource-container-intro">
+                    Kies uit onderstaand overzicht jouw favoriete ontwerp voor het
+                    kunstwerk voor de Aletta Jacobsbuurt, en vul in de volgende
+                    stap je gegevens in.
+                  </p>
+                  <Spacer size={2} />
+                  <h5>Kies een ontwerp</h5>
+                  <Spacer size={1} />
+                </>
+              )}
+              renderItem={(resource) => (
+                <>
+                  <Image
+                    src={resource.images[0]}
+                    imageFooter={
+                      <>
+                        <PlainButton>Lees meer</PlainButton>
+                        <SecondaryButton onClick={() => setResource(resource)}>
+                          Stem
+                        </SecondaryButton>
+                      </>
+                    }
+                  />
+                </>
+              )}
+              {...props}
+              allowFiltering={false}
+            />
+            <div className="stem-module-resource-footer">
+              <SecondaryButton
+                disabled={!resource}
+                onClick={() => setCurrentStep(currentStep + 1)}>
+                Volgende
+              </SecondaryButton>
+            </div> 
+          </div>
         ) : null}
 
         {currentStep === 1 && resource ? (
-          <section>
-            <p>
-              Via onderstaande knop vul je je emailadres in. Ter controle krijg
-              je een link toegestuurd om je e-mailadres te bevestigen. Als dat
-              lukt kom je terug op deze pagina om je stem definitief in te
-              sturen.
-            </p>
-            <Spacer size={1} />
-            <SecondaryButton>Vul je stemcode in</SecondaryButton>
-            <Spacer size={1} />
-            <h6>Uw stem.</h6>
-            <div className="osc-image-resizer">
-              <Image src={resource.images[0]} />
+          <>
+            <section className="stem-module-resource-container">
+              <p>
+                Via onderstaande knop vul je je emailadres in. Ter controle krijg
+                je een link toegestuurd om je e-mailadres te bevestigen. Als dat
+                lukt kom je terug op deze pagina om je stem definitief in te
+                sturen.
+              </p>
+              <Spacer size={1} />
+              <SecondaryButton>Vul je stemcode in</SecondaryButton>
+              <Spacer size={2} />
+              <h5>Uw stem.</h5>
+              <Spacer size={1} />
+              <div className="osc-image-resizer">
+                <Image src={resource.images[0]} />
+              </div>
+            </section>
+            <div className="stem-module-resource-footer">
+              <PlainButton onClick={() => setCurrentStep(currentStep - 1)}>
+                Vorige
+              </PlainButton>
+              <SecondaryButton 
+              className="stem-module-send-button"
+              onClick={() => setCurrentStep(currentStep + 1)}>
+                Verstuur
+              </SecondaryButton>
             </div>
-          </section>
+          </>
         ) : null}
 
         {currentStep === 2 ? (
-          <section>
-            <h6>Mail is verstuurd</h6>
+          <section className="stem-module-resource-container">
+            <h5>Mail is verstuurd</h5>
             <p>Kijk in je mailbox</p>
           </section>
         ) : null}
 
         {currentStep >= 3 ? (
-          <section>
-            <h6>Gelukt, je stem is opgeslagen!</h6>
-            <p>
-              Bedankt voor het stemmen! Hou deze site in de gaten voor de
-              uitslag.
-            </p>
-            <Spacer size={1} />
+          <>
+            <section className="stem-module-resource-container">
+              <div
+                className={'confirm-icon'}>
+                <p>✓</p>
+              </div>
+              <Spacer size={1} />
+              <h5>Gelukt, je stem is opgeslagen!</h5>
+              <p>
+                Bedankt voor het stemmen! Hou deze site in de gaten voor de
+                uitslag.
+              </p>
+              <Spacer size={1} />
+            </section>
+            <div className="stem-module-resource-footer">
             <SecondaryButton
-              onClick={() => (window.location.href = props.site)}>
-              Klaar
-            </SecondaryButton>
-          </section>
-        ) : null}
-
-        {currentStep === 2 && resource ? <section></section> : null}
-        <Spacer size={1} />
-        {currentStep > 0 && currentStep < 2 ? (
-          <PlainButton onClick={() => setCurrentStep(currentStep - 1)}>
-            Vorige
-          </PlainButton>
-        ) : null}
-        {currentStep < 2 ? (
-          <SecondaryButton
-            disabled={(currentStep === 0 && !resource) || currentStep > 1}
-            onClick={() => setCurrentStep(currentStep + 1)}>
-            Volgende
-          </SecondaryButton>
+                onClick={() => (window.location.href = props.site)}>
+                Klaar
+              </SecondaryButton>
+            </div>
+          </>
         ) : null}
       </div>
     </>
