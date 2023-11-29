62f3a735e5c8caa0aa521b6d9bf4ea97fbacb161
BartWtje <bartw.tje@hotmail.com>
2023-11-29 15:27:09 +0100

    Slider werkend op safari browser

--- packages/ui/src/slider/index.tsx
+++ packages/ui/src/slider/index.tsx
@@ -9,11 +9,30 @@ type Props = React.InputHTMLAttributes<HTMLInputElement> & {
 };
 
 const Slider = forwardRef<HTMLInputElement, Props>((props, ref) => {
+  const min = 0;
+  const max = 100;
+  const [value, setValue] = React.useState(50);
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setValue(Number(e.currentTarget.value));
+  }
+
+  function calcProgress(min: number, max: number, value: number) {
+    const range = max - min;
+    const progInRange = value - min;
+    return ((progInRange*2)/range)-1;
+  }
+
   return (
-    <div>
-      <div className={`osc-slider`}>
-        <input type='range' min={0} max={100} step={1} className='osc-slider-input-range'/>
+    <div className={`osc-slider`}>
+      <div className="osc-slider-track-container">
+        <div className="osc-slider-track"></div>
+        <div className="osc-slider-track-progress-container">
+          <div className="osc-slider-track-progress" style={{ transform: 'scaleX(' + calcProgress(min, max, value) + ')' }}></div>
+        </div>
+        <div className="osc-slider-track-dot-start osc-slider-track-dot-center"></div>
       </div>
+      <input type='range' min={min} max={max} step={1} defaultValue={value} onChange={onChange} className={'osc-slider-input-range'}/>
     </div>
   );
 });
